0fbc3bb3e8d1dc201cb4834eb0472f2be9100f9d
Andi Everitt <andi@raje.org.uk>
2013-09-09 08:42:07 +0100

    Split out integration tests to separate module. This is necessary as the current setup means the integration tests run against the last installed (to the maven repository) version of the plugin and NOT the one just being built.

--- msbuild-maven-plugin-it/src/test/java/uk/org/raje/maven/plugin/msbuild/it/MavenITHelloWorldBuildTest.java
+++ msbuild-maven-plugin-it/src/test/java/uk/org/raje/maven/plugin/msbuild/it/MavenITHelloWorldBuildTest.java
@@ -31,33 +31,36 @@ import static uk.org.raje.maven.plugin.msbuild.it.MSBuildMojoITHelper.calculateA
 import static uk.org.raje.maven.plugin.msbuild.it.MSBuildMojoITHelper.checkProjectBuildOutputIsCleaned;
 
 /**
- * Integration test that runs the hello-world-lib-test
+ * Integration test that runs the hello-world-build-test
  *
  */
-public class MavenITHelloWorldLibTest 
+public class MavenITHelloWorldBuildTest 
 {
 
     @Test
     public void testSolutionBuild() throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(),
-                "/it/hello-world-lib-test" );
+                "/hello-world-build-test" );
         File releaseDir = calculateAndDeleteOutputDirectory( testDir, "Release" );
         File debugDir = calculateAndDeleteOutputDirectory( testDir, "Debug" );
 
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
         
         // Delete any existing artifact from the local repository
-        verifier.deleteArtifact( GROUPID, SOLUTION_ARTIFACTID, VERSION, "lib" );
+        verifier.deleteArtifact( GROUPID, SOLUTION_ARTIFACTID, VERSION, "exe" );
 
         verifier.executeGoal( "install" );
         verifier.verifyErrorFreeLog();
         List<String> releaseDirContents = Arrays.asList( releaseDir.list() );
-        assertEquals( 1, releaseDirContents.size() );
-        assertTrue( releaseDirContents.contains( "hello-world-lib.lib" ) ); 
+        assertEquals( 2, releaseDirContents.size() );
+        assertTrue( releaseDirContents.contains( "hello-world.exe" ) ); 
+        assertTrue( releaseDirContents.contains( "hello-world.pdb" ) ); 
         List<String> debugDirContents = Arrays.asList( debugDir.list() );
-        assertEquals( 1, debugDirContents.size() );
-        assertTrue( debugDirContents.contains( "hello-world-lib.lib" ) ); 
+        assertEquals( 3, debugDirContents.size() );
+        assertTrue( debugDirContents.contains( "hello-world.exe" ) ); 
+        assertTrue( debugDirContents.contains( "hello-world.ilk" ) ); 
+        assertTrue( debugDirContents.contains( "hello-world.pdb" ) ); 
         
         verifier.resetStreams();
         
@@ -68,48 +71,90 @@ public class MavenITHelloWorldLibTest
     }
 
     @Test
+    public void testSolutionClean() throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(),
+                "/hello-world-build-test" );
+        File releaseDir = new File( testDir, "Release" );
+        File debugDir = new File( testDir, "Debug" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        
+        verifier.executeGoal( "clean" );
+        verifier.verifyErrorFreeLog();
+        if ( releaseDir.exists() )
+        {
+            assertEquals( 0, releaseDir.list().length );
+        }
+        if ( debugDir.exists() )
+        {
+            assertEquals( 0, debugDir.list().length );
+        }
+    }
+
+    @Test
     public void testProjectBuild() throws Exception
     {
         File testDir = ResourceExtractor.simpleExtractResources( getClass(),
-                "/it/hello-world-lib-test/hello-world-lib" );
+                "/hello-world-build-test/hello-world" );
         File releaseDir = calculateAndDeleteOutputDirectory( testDir, "Release" );
         File debugDir = calculateAndDeleteOutputDirectory( testDir, "Debug" );
 
         Verifier verifier = new Verifier( testDir.getAbsolutePath() );
         
         // Delete any existing artifact from the local repository
-        verifier.deleteArtifact( GROUPID, PROJECT_ARTIFACTID, VERSION, "lib" );
+        verifier.deleteArtifact( GROUPID, PROJECT_ARTIFACTID, VERSION, "exe" );
 
         verifier.executeGoal( "install" );
         verifier.verifyErrorFreeLog();
         List<String> releaseDirContents = Arrays.asList( releaseDir.list() );
         assertEquals( HELLOWORLD_PROJECT_RELEASE_FILE_COUNT, releaseDirContents.size() );
-        // We don't check all 10 files, just the most important ones
-        assertTrue( releaseDirContents.contains( "hello-world-lib.lib" ) ); 
+        // We don't check all 16 files, just the most important ones
+        assertTrue( releaseDirContents.contains( "hello-world.exe" ) ); 
+        assertTrue( releaseDirContents.contains( "hello-world.pdb" ) ); 
         List<String> debugDirContents = Arrays.asList( debugDir.list() );
         assertEquals( HELLOWORLD_PROJECT_DEBUG_FILE_COUNT, debugDirContents.size() );
-        // We don't check all 11 files, just the most important ones
-        assertTrue( debugDirContents.contains( "hello-world-lib.lib" ) ); 
+        // We don't check all 30 files, just the most important ones
+        assertTrue( debugDirContents.contains( "hello-world.exe" ) ); 
+        assertTrue( debugDirContents.contains( "hello-world.ilk" ) ); 
+        assertTrue( debugDirContents.contains( "hello-world.pdb" ) ); 
         
         verifier.assertArtifactPresent( GROUPID,
                 PROJECT_ARTIFACTID,
                 VERSION, 
-                MSBuildPackaging.LIB );
+                MSBuildPackaging.EXE );
         // NOTE: verifier doesn't appear to provide a way to check artifacts with other classifiers
 
         verifier.resetStreams();
         
         verifier.executeGoal( "clean" );
         verifier.verifyErrorFreeLog();
-        checkProjectBuildOutputIsCleaned( "hello-world-lib", releaseDir );
-        checkProjectBuildOutputIsCleaned( "hello-world-lib", debugDir );
+        checkProjectBuildOutputIsCleaned( "hello-world", releaseDir );
+        checkProjectBuildOutputIsCleaned( "hello-world", debugDir );
     }
 
+    @Test
+    public void testProjectClean() throws Exception
+    {
+        File testDir = ResourceExtractor.simpleExtractResources( getClass(),
+                "/hello-world-build-test/hello-world" );
+        File releaseDir = new File( testDir, "Release" );
+        File debugDir = new File( testDir, "Debug" );
+
+        Verifier verifier = new Verifier( testDir.getAbsolutePath() );
+        
+        verifier.executeGoal( "clean" );
+        verifier.verifyErrorFreeLog();
+        checkProjectBuildOutputIsCleaned( "hello-world", releaseDir );
+        checkProjectBuildOutputIsCleaned( "hello-world", debugDir );
+    }
+
+
     private static final String GROUPID = "uk.org.raje.maven.plugins.msbuild.it";
-    private static final String SOLUTION_ARTIFACTID = "hello-world-lib-solution-test";
-    private static final String PROJECT_ARTIFACTID = "hello-world-lib-project-test";
+    private static final String SOLUTION_ARTIFACTID = "hello-world-build-solution-test";
+    private static final String PROJECT_ARTIFACTID = "hello-world-build-project-test";
     private static final String VERSION = "1-SNAPSHOT";
 
-    private static final int HELLOWORLD_PROJECT_RELEASE_FILE_COUNT = 10;
-    private static final int HELLOWORLD_PROJECT_DEBUG_FILE_COUNT = 11;
+    private static final int HELLOWORLD_PROJECT_RELEASE_FILE_COUNT = 15;
+    private static final int HELLOWORLD_PROJECT_DEBUG_FILE_COUNT = 29;
 }
