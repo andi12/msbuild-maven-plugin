1e118a589e0f7f0e0412f1bdd68246c795083648
Daniele Masato <masato.daniele@gmail.com>
2013-10-25 17:17:12 +0100

    Added integration tests for Vera++ coding style analysis tool

---
+++ msbuild-maven-plugin-it/src/test/resources/compute-pi-vera-test/compute-pi/compute-pi-app.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "compute-pi.h"
+
+
+int main(int argc, char* argv[]) {
+    printf("The approximate value of PI is %f\n", computePI(COMPUTE_PI_ITERATIONS));
+    return 0;
+}
